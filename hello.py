d001d4a316aa48d7242191f0da5d1509a0e954ce
Akanshu Aich <b221008@iiit-bh.ac.in>
2025-05-08 14:53:53 +0530

    Fix code_quality_issues in hello.py: The use of `eval` is a security risk as it can execute arbitrary code. In this c

--- hello.py
+++ hello.py
@@ -3,14 +3,14 @@ import sys
 import pickle
  
 def do_something(input):
-  eval("print('Chapri')")  # Security issue: use of eval
+  print('Chapri')  # Security issue: use of eval
   file = open("somefile.txt", "w")  # Code quality: not using with statement
   file.write("hello world\n")
   file.close()
   if input = "yes":  # Potential bug: wrong comparison operator
     print("You said yes")
   elif input == "no":
-        print("You said no")  # Style violation: inconsistent indentation & spacing
+        print( "You said no")  # Style violation: inconsistent indentation & spacing
   else:
         print("Invalid input")
   result = 0
