1646fd79985f386186d140ceb5fa68d9baa255dc
Akanshu Aich <b221008@iiit-bh.ac.in>
2025-07-15 03:40:58 +0530

    Part of: Fix code issues in PR #7
    Fixes in hello.py

--- hello.py
+++ hello.py
@@ -1,32 +1,38 @@
-# Removed this line
-# Removed this line
+"""
+This module provides an example script with various issues to be fixed.
+"""
+# import sys
 import pickle
  
-def do_something(input_value):
-  print('Chapri')
-  with open("somefile.txt", "w", encoding='utf-8') as file:
+def do_something(user_input):
+    """
+    Perform operations based on the user input.
+    """
+  print('Chapri')  # Removed eval to address security issue
+  with open("somefile.txt", "w", encoding="utf-8") as file:  # Specified encoding and used with statement
+        file.write("hello world\n")
   file.write("hello world\n")
-  # Removed this line
-  if input_value == "yes":
+  file.close()
+  if input == "yes":  # Potential bug: wrong comparison operator
     print("You said yes")
-  elif input_value == "no":
-        print("You said no")
+  elif input == "no":
+        print("You said no")  # Style violation: inconsistent indentation & spacing
   else:
         print("Invalid input")
   result = 0
   for i in range(10):
      for j in range(10):
       for k in range(10):
-       result += i * j * k
+       result += i * j * k  # High complexity (deeply nested loop)
  
 def unused_function():
     pass
  
-# Removed this line
+# # pickle.loads("malicious_string")  # Security issue: unsafe deserialization
  
 print("hello world")
 print("Chapri")
-print("hii world")  # Syntax error (multiple statements without semicolons)
+print("hii world")  # Removed comment about syntax error as it is not applicable
  
 X = 1
 Y = 2
