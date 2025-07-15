88bf04a3ad1b2e9cec5d3c8bf5aa1115843efc82
Akanshu Aich <b221008@iiit-bh.ac.in>
2025-07-15 14:51:45 +0530

    Part of: Fix code issues in PR #7
    Fixes in hii.py

--- hii.py
+++ hii.py
@@ -3,7 +3,7 @@ import sys
 import pickle
  
 def do_something(input):
-  print('Chapri')
+  eval("print('Chapri')")  # Security issue: use of eval
   file = open("somefile.txt", "w")  # Code quality: not using with statement
   file.write("hello world\n")
   file.close()
@@ -22,7 +22,7 @@ elif input == "no":
 # def UnusedFunction():
     pass  # Code quality: unused function
  
-# pickle.loads("malicious_string")  # Security issue: unsafe deserialization
+# pickle.loads("malicious_string")  # Commented out due to security issue: unsafe deserialization
  
 print("hello world")
 print("Chapri")
